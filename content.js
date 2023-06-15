59ea01aa8d8c8648c2936bd9645be34fa222b44e
Nitish Parkar <developer.nitish@gmail.com>
2023-06-16 00:57:31 +0530

    Cleanup and refactor code
    - extract html selectors as constants
    - extract helper functions for buttons
    - extract openai api call into a function
    - add basic error handling

--- content.js
+++ content.js
@@ -1,26 +1,38 @@
-// content.js
+const SELECTOR_CHAT_LIST = '[aria-label="Chat list"]';
+const SELECTOR_INPUT_BOX = '#main [data-testid="conversation-compose-box-input"]';
+const SELECTOR_CHAT_ROW = '[role="application"] [role="row"]';
+const SUGGEST_BUTTON_ID = 'nm-suggest';
+const ENHANCE_BUTTON_ID = 'nm-enhance';
 
-var OPENAI_KEY = '';
+let OPENAI_KEY = '';
 
-function documentLoadedListener() {
-  var ele = document.querySelector('[aria-label="Chat list"]');
-  ele.addEventListener("click", function () {
-    createSuggestButton();
-    createEnhanceButton();
+// Wait for Whatsapp to load
+const targetNode = document.getElementById('app');
+const observer = new MutationObserver(function (mutations) {
+  if (document.querySelector(SELECTOR_CHAT_LIST)) {
+    observer.disconnect();
+    initApp();
+  }
+});
+observer.observe(targetNode, { subtree: true, childList: true });
 
-    var el = document.querySelector('#main [data-testid="conversation-compose-box-input"]');
-    const suggest = document.getElementById('nm-suggest');
-    const enhance = document.getElementById('nm-enhance');
+function initApp() {
+  const chatsList = document.querySelector(SELECTOR_CHAT_LIST);
+  chatsList.addEventListener("click", function () {
+    const suggestButton = createSuggestButton();
+    const enhanceButton = createEnhanceButton();
+
+    const el = document.querySelector(SELECTOR_INPUT_BOX);
     el.addEventListener('keyup', () => {
       if (el.innerText.trim().length === 0) {
-        if (suggest.style.display === 'none') {
-          suggest.style.display = 'block';
-          enhance.style.display = 'none';
+        if (suggestButton.style.display === 'none') {
+          suggestButton.style.display = 'block';
+          enhanceButton.style.display = 'none';
         }
       } else {
-        if (enhance.style.display === 'none') {
-          enhance.style.display = 'block';
-          suggest.style.display = 'none';
+        if (enhanceButton.style.display === 'none') {
+          enhanceButton.style.display = 'block';
+          suggestButton.style.display = 'none';
         }
       }
     });
@@ -32,199 +44,166 @@ function documentLoadedListener() {
 }
 
 function createSuggestButton() {
-  const inputField = document.querySelector('footer .lexical-rich-text-input');
+  const inputField = document.querySelector(SELECTOR_INPUT_BOX);
   if (!inputField) return;
 
-  const button = document.createElement('button');
-  button.id = 'nm-suggest'
-  button.innerText = 'Suggest';
-
-  // Add the desired CSS properties to the button
-  button.style.position = 'absolute';
-  button.style.top = '0px';
-  button.style.right = '0px';
-  button.style.color = '#25D366';
-  button.style.padding = '4px 5px';
-  button.style.borderRadius = '9px';
-  button.style.border = '1px solid #8696a0';
-  button.style.cursor = 'pointer';
-
+  const button = createActionButton(SUGGEST_BUTTON_ID);
   button.addEventListener('click', () => {
-    button.disabled = true;
-    button.innerText = 'Talking to AI...';
-    button.style.border = 'none';
-    button.style.cursor = 'wait';
+    disableButtonForProcessing(button);
 
-    var outs = []
-    var rows = document.querySelectorAll('[role="application"] [role="row"]');
-    for (row of rows) {
-      var ct = row.querySelector('span.copyable-text')
+    const messageTexts = [];
+    const rows = document.querySelectorAll(SELECTOR_CHAT_ROW);
+    for (const row of rows) {
+      const ct = row.querySelector('span.copyable-text');
       if (ct) {
-        var sender = row.querySelector('.message-out') ? 'Me:' : row.querySelector('[data-pre-plain-text]').getAttribute('data-pre-plain-text').split('] ')[1]
-        outs.push(` ${sender} ${ct.innerText}`)
+        const sender = row.querySelector('.message-out') ? 'Me:' : row.querySelector('[data-pre-plain-text]').getAttribute('data-pre-plain-text').split('] ')[1];
+        messageTexts.push(` ${sender} ${ct.innerText}`);
       }
     }
 
-    var systemPrompt = `Given the message exchange provided below, your task is to generate an appropriate response to the message from 'Me:'. Each message in the exchange begins with the name of the sender, followed by a colon (':'), and then the actual message.
-Please consider the context of the conversation and generate a response that is relevant and coherent. Your response should only include the text of the message, without adding 'Me:' at the beginning.`
-    var messages = outs.map(content => ({ role: 'user', content: content }));
-    messages.unshift({ role: 'system', content: systemPrompt })
-    // console.log('Messages:')
-    // console.log(messages)
-    // console.log('----------')
+    const messages = messageTexts.map(content => ({ role: 'user', content: content }));
+    const systemPrompt = `Given the message exchange provided below, your task is to generate an appropriate response to the message from 'Me:'. Each message in the exchange begins with the name of the sender, followed by a colon (':'), and then the actual message.
+Please consider the context of the conversation and generate a response that is relevant and coherent. Your response should only include the text of the message, without adding 'Me:' at the beginning.`;
+    messages.unshift({ role: 'system', content: systemPrompt });
 
-    fetch('https://api.openai.com/v1/chat/completions', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${OPENAI_KEY}`,
-      },
-      body: JSON.stringify({
-        "model": "gpt-3.5-turbo",
-        "messages": messages,
-        "temperature": 0.5
-      }),
-    })
+    callOpenAIAPI(messages)
       .finally(() => {
-        button.disabled = false;
-        button.innerText = 'Suggest';
-        button.style.border = '1px solid #8696a0';
-        button.style.cursor = 'pointer';
+        enableButtonAfterProcessing(button);
       })
-      .then((res) => res.json())
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+
+        throw new Error('OpenAI API error');
+      })
       .then((data) => {
         if (data && data.choices && data.choices.length > 0) {
-          const responseMessage = data.choices[0].message.content.trim();
-          // console.log('OpenAI Response:')
-          // console.log(responseMessage)
-          // console.log('----------')
+          const aiReply = data.choices[0].message.content.trim();
 
           // https://gist.github.com/kevinresol/f5253d148d5a37201b3e53f2b4fa70b2
-          var ip = document.querySelector('#main [data-testid="conversation-compose-box-input"]');
+          const ip = document.querySelector(SELECTOR_INPUT_BOX);
           ip.dispatchEvent(new InputEvent('input', {
             inputType: 'insertText',
-            data: responseMessage,
+            data: aiReply,
             bubbles: true,
             cancelable: false,
             composed: true,
             detail: 0,
           }));
+
           // To hide suggest and show enhance
           setTimeout(() => ip.dispatchEvent(new KeyboardEvent('keyup', { })), 100);
         }
       })
       .catch((error) => {
+        alert('Could not talk to AI. Please try again.');
         console.error('Error:', error);
       });
   });
 
-  inputField.appendChild(button);
+  inputField.parentElement.appendChild(button);
+  return button;
 }
 
 function createEnhanceButton() {
-  const inputField = document.querySelector('footer .lexical-rich-text-input');
+  const inputField = document.querySelector(SELECTOR_INPUT_BOX);
   if (!inputField) return;
 
-  const button = document.createElement('button');
-  button.id = 'nm-enhance'
-  button.innerText = 'Enhance';
-
-  // Add the desired CSS properties to the button
-  button.style.position = 'absolute';
-  button.style.top = '0px';
-  button.style.right = '0px';
-  button.style.color = '#25D366';
-  button.style.padding = '4px 5px';
-  button.style.borderRadius = '9px';
-  button.style.border = '1px solid #8696a0';
-  button.style.display = 'none';
-  button.style.cursor = 'pointer';
-
+  const button = createActionButton(ENHANCE_BUTTON_ID);
   button.addEventListener('click', () => {
-    button.disabled = true;
-    button.innerText = 'Talking to AI...';
-    button.style.border = 'none';
-    button.style.cursor = 'wait';
+    disableButtonForProcessing(button);
 
-    var outs = []
-    var rows = document.querySelectorAll('[role="application"] [role="row"]');
-    for (row of rows) {
-      var ct = row.querySelector('span.copyable-text')
-      if (ct) {
-        var sender = row.querySelector('.message-out') ? 'Me:' : row.querySelector('[data-pre-plain-text]').getAttribute('data-pre-plain-text').split('] ')[1]
-        outs.push(` ${sender} ${ct.innerText}`)
-      }
-    }
-
-    var messages = []//outs.map(content => ({ role: 'user', content: content }));
-
-    var userWrittenReply = document.querySelector('#main [data-testid="conversation-compose-box-input"]').innerText;
-    var systemPrompt = `Please proofread and improve the user's message delimited by ---. Your task is to make the message sound simple and coherent. Correct any grammatical mistakes if present.
+    const userWrittenReply = document.querySelector(SELECTOR_INPUT_BOX).innerText;
+    const systemPrompt = `Please proofread and improve the user's message delimited by ---. Your task is to make the message sound simple and coherent. Correct any grammatical mistakes if present.
 ---
 ${userWrittenReply}
 ---
-Focus on enhancing clarity, coherence, and grammar while refining the user's response.`
-// Preceding message exchange is provided below only for the context. Each message in the exchange starts with the sender's name, followed by a colon (':'), and then the message content. Do not try to proofread or improve these messages.`
-    messages.unshift({ role: 'system', content: systemPrompt })
-    // console.log('Messages:')
-    // console.log(messages)
-    // console.log('----------')
+Focus on enhancing clarity, coherence, and grammar while refining the user's response.`;
 
-    fetch('https://api.openai.com/v1/chat/completions', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${OPENAI_KEY}`,
-      },
-      body: JSON.stringify({
-        "model": "gpt-3.5-turbo",
-        "messages": messages,
-        "temperature": 0.5
-      }),
-    })
+    const messages = [{ role: 'system', content: systemPrompt }];
+
+    callOpenAIAPI(messages)
       .finally(() => {
-        button.disabled = false;
-        button.innerText = 'Enhance';
-        button.style.border = '1px solid #8696a0';
-        button.style.cursor = 'pointer';
+        enableButtonAfterProcessing(button);
       })
-      .then((res) => res.json())
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+
+        throw new Error('OpenAI API error');
+      })
       .then((data) => {
         if (data && data.choices && data.choices.length > 0) {
-          const responseMessage = data.choices[0].message.content.trim();
-          console.log('OpenAI Response:')
-          console.log(responseMessage)
-          console.log('----------')
+          const aiReply = data.choices[0].message.content.trim();
 
           // https://stackoverflow.com/a/73029608
           const dataTransfer = new DataTransfer();
-          dataTransfer.setData('text', userWrittenReply + '\n' + responseMessage);
+          dataTransfer.setData('text', userWrittenReply + '\n' + aiReply);
           const evt = new ClipboardEvent('paste', {
             clipboardData: dataTransfer,
             bubbles: true
           });
-          var el = document.querySelector('#main [data-testid="conversation-compose-box-input"]');
+          const el = document.querySelector(SELECTOR_INPUT_BOX);
           el.focus();
           document.execCommand("selectall");
           el.dispatchEvent(evt);
         }
       })
       .catch((error) => {
+        alert('Could not talk to AI. Please try again.');
         console.error('Error:', error);
       });
   });
 
-  inputField.appendChild(button);
+  inputField.parentElement.appendChild(button);
+  return button;
 }
 
+function createActionButton(id) {
+  const button = document.createElement('button');
+  button.id = id;
+  button.innerText = (id === SUGGEST_BUTTON_ID) ? 'Suggest' : 'Enhance';
 
-const targetNode = document.getElementById("app");
+  button.style.position = 'absolute';
+  button.style.top = '0px';
+  button.style.right = '0px';
+  button.style.color = '#25D366';
+  button.style.padding = '4px 5px';
+  button.style.borderRadius = '9px';
+  button.style.border = '1px solid #8696a0';
+  button.style.display = (id === SUGGEST_BUTTON_ID) ? 'block' : 'none';
+  button.style.cursor = 'pointer';
 
-var observer = new MutationObserver(function (mutations) {
-  if (document.querySelector('[aria-label="Chat list"]')) {
-    observer.disconnect();
-    documentLoadedListener();
-  }
-});
+  return button;
+}
 
-observer.observe(targetNode, { attributes: false, childList: true, characterData: false, subtree: true });
+function disableButtonForProcessing(button) {
+  button.disabled = true;
+  button.innerText = 'Talking to AI...';
+  button.style.border = 'none';
+  button.style.cursor = 'wait';
+}
+
+function enableButtonAfterProcessing(button) {
+  button.disabled = false;
+  button.innerText = 'Suggest';
+  button.style.border = '1px solid #8696a0';
+  button.style.cursor = 'pointer';
+}
+
+function callOpenAIAPI(messages) {
+  return fetch('https://api.openai.com/v1/chat/completions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${OPENAI_KEY}`,
+    },
+    body: JSON.stringify({
+      "model": "gpt-3.5-turbo",
+      "messages": messages,
+      "temperature": 0.5
+    }),
+  });
+}
